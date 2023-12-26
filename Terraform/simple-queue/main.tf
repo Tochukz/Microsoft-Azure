1b9f45777b0d6999d9b017843b8eca7a618bb4f3
Tochukwu Nwachukwu <truetochukz@gmail.com>
2023-12-26 22:16:22 +0200

    Storage Queue

--- Terraform/simple-queue/main.tf
+++ Terraform/simple-queue/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_storage_account" "simple_storage_account" {
   resource_group_name      = azurerm_resource_group.simple_queue_rg.name
   location                 = azurerm_resource_group.simple_queue_rg.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS" # use ZRS or GRS for production
   min_tls_version          = "TLS1_2"
   tags = {
     Environment = "Development"
