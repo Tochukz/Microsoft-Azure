09c0d68b2ee3f2360caa6a848315a8b0f107fb70
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-01-27 16:19:28 +0200

    Linux-Node WebApp

--- Terraform/linux-node-webapp/main.tf
+++ Terraform/linux-node-webapp/main.tf
@@ -73,11 +73,56 @@ resource "azurerm_linux_web_app" "simple_webapp" {
   }
 }
 
-# Optional resource, may be omitted.
+# Optional resource, may be omitted. If provided, will deploy code from the repo to the webapp.
 resource "azurerm_app_service_source_control" "code_source" {
   app_id                 = azurerm_linux_web_app.simple_webapp.id
   repo_url               = "https://github.com/Tochukz/simple-express"
   branch                 = "master"
   use_manual_integration = true
   use_mercurial          = false
+}
+
+
+resource "azurerm_storage_account" "simple_storage" {
+  name                     = "simplestore${random_integer.simple_int.result}"
+  resource_group_name      = azurerm_resource_group.simple_rg.name
+  location                 = azurerm_resource_group.simple_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_log_analytics_workspace" "simple_analytics" {
+  name                = "WebAnalytics${random_integer.simple_int.result}"
+  resource_group_name = azurerm_resource_group.simple_rg.name
+  location            = azurerm_resource_group.simple_rg.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_diagnostic_setting" "simple_monitor" {
+  name               = "simplemonitor${random_integer.simple_int.result}"
+  target_resource_id = azurerm_linux_web_app.simple_webapp.id
+  # storage_account_id = azurerm_storage_account.simple_storage.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.simple_analytics.id
+  log {
+    category = "AppServiceAppLogs"
+    enabled  = true
+  }
+  log {
+    category = "AppServiceHTTPLogs"
+    enabled  = true
+  }
+  log {
+    category = "AppServiceConsoleLogs"
+    enabled  = true
+  }
+  log {
+    category = "AppServicePlatformLogs"
+    enabled  = true
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
 }
